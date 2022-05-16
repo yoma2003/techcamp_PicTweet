1ed7d13d40d85266c80604c551043bbcf1f37055
yoma2003 <yoma.xxx2003@gmail.com>
2022-05-16 19:06:22 +0900

    finish

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show] 
+  before_action :move_to_index, except: [:index, :show, :search]
   
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -11,6 +11,7 @@ class TweetsController < ApplicationController
   end
 
   def create
+    binding.pry
     Tweet.create(tweet_params)
   end
 
@@ -30,6 +31,10 @@ class TweetsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
   private
