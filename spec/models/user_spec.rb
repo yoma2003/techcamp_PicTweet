c37e9d32109acb63851e15c2a13a07f2f4346a06
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-05-18 19:21:37 +0900

    Userモデルのバリデーションテストを更新

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -15,6 +18,44 @@ RSpec.describe User, type: :model do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it 'nicknameが7文字以上では登録できない' do
+      @user.nickname = "0000000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 6 characters)")
+    end
+    it '重複したemailが存在する場合は登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+    it 'emailは@を含まないと登録できない' do
+      @user.email = 'testexample'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
+    it 'passwordが5文字以下では登録できない' do
+      @user.password = @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it 'passwordが129文字以上では登録できない' do
+      @user.password = @user.password_confirmation = '0' * 129
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
     end
   end
 end
