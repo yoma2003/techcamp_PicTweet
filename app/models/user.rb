a366dc0b509ad19af7834a961ce989ef98a17f63
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-05-18 14:31:17 +0900

    Userモデルにnicknameのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
-  has_many :comments 
+  has_many :comments
+
+  validates :nickname, presence: true
 end
